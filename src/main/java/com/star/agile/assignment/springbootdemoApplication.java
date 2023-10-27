003b6ee02bd1a241c4446bdade600adaaa01c465
suvarnabhoi <suvarnabhoi98@gmail.com>
2023-10-27 22:07:03 +0530

    first commit

---
+++ src/main/java/com/star/agile/assignment/springbootdemoApplication.java
@@ -0,0 +1,13 @@
+package com.star.agile.assignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class springbootdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(springbootdemoApplication.class, args);
+	}
+
+}
